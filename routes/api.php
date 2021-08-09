914818aee7d51f30dd901ba2a517dbe33f3e1287
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2021-08-09 14:34:09 -0300

    alterações em routes

--- routes/api.php
+++ routes/api.php
@@ -15,23 +15,32 @@ Route::get('/unauthenticated', function() {
     return ['error' => 'Usuário não está logado!'];
 })->name('login');
 
-Route::middleware('auth:api')->post('/user', [AuthController::class, 'create']); //insere novo 
 Route::post('/auth/login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->post('/auth/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:api')->get('/auth/me', [AuthController::class, 'me']); //pegar o user logado
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
 
-Route::middleware('auth:api')->post('/category', [CategoryController::class, 'create']);
-Route::middleware('auth:api')->get('/categories', [CategoryController::class, 'readAll']);
-Route::middleware('auth:api')->get('/category/{id}', [CategoryController::class, 'read']);
-Route::middleware('auth:api')->put('/category/{id}', [CategoryController::class, 'update']);
-Route::middleware('auth:api')->delete('/category/{id}', [CategoryController::class, 'delete']);
+    Route::post('/user', [AuthController::class, 'create']); //insere novo 
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    
+    Route::get('/auth/me', [AuthController::class, 'me']); //pegar o user logado
 
-Route::middleware('auth:api')->post('/product', [ProductController::class, 'create']);
-Route::middleware('auth:api')->get('/products', [ProductController::class, 'readAll']);
-Route::middleware('auth:api')->get('/product/{id}', [ProductController::class, 'read']);
-Route::middleware('auth:api')->put('/product/{id}', [ProductController::class, 'update']);
-Route::middleware('auth:api')->delete('/product/{id}', [ProductController::class, 'delete']);
+    Route::post('/category', [CategoryController::class, 'create']);
+    Route::get('/categories', [CategoryController::class, 'readAll']);
+    Route::get('/category/{id}', [CategoryController::class, 'read']);
+    Route::put('/category/{id}', [CategoryController::class, 'update']);
+    Route::delete('/category/{id}', [CategoryController::class, 'delete']);
+
+    Route::post('/product', [ProductController::class, 'create']);
+    Route::get('/products', [ProductController::class, 'readAll']);
+    Route::get('/product/{id}', [ProductController::class, 'read']);
+    Route::put('/product/{id}', [ProductController::class, 'update']);
+    Route::delete('/product/{id}', [ProductController::class, 'delete']);
+    
+});
+
 
 
 
